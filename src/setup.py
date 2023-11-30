c1d232a4e9a55626a97a0f974b9540006faaf54e
scottdoy <scottdoy@buffalo.edu>
2023-11-30 11:49:16 -0500

    Add find_packages to setup

--- src/setup.py
+++ src/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sunycell", 
+    packages=find_packages(),
     description="Toolkit for working with the SUNYCell image repository.",
     license='Apache Software License 2.0',
     version='0.1a1',
