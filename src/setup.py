efe947dea796a3cf68e5e02ec4f267daf1d64645
scottdoy <scottdoy@buffalo.edu>
2023-12-01 09:43:43 -0500

    Set up semver on setup

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     description="Toolkit for working with the SUNYCell image repository.",
     license='Apache Software License 2.0',
-    version='0.1a1',
+    version='0.1.0',
     install_requires=[
         'histomicstk',
         'python-dotenv',
